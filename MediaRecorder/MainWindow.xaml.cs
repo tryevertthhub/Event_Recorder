4e86943feedb3f9cd99bc594a4ecbd5066f0abb6
tryevertthhub <tryevertth@gmail.com>
2024-10-24 01:54:38 +0000

    init: init project

---
+++ MediaRecorder/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows;
+using Gma.System.MouseKeyHook;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace MediaRecorder
+{
+    public partial class MainWindow : Window
+    {
+       
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            RegisterHotkeys();
+            CreateDataDirectory(); // Ensure the data directory exists
+        }
+
+    }
+
+}
